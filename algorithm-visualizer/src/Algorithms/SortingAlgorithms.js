b620ead1bc1faa388e02a4d604d60757ac7e5ce8
Maxwell-Calhoun <mcalh16@gmail.com>
2023-12-15 00:10:07 -0700

    InsertionSort is functional and behaving as expected

---
+++ algorithm-visualizer/src/Algorithms/SortingAlgorithms.js
@@ -0,0 +1,27 @@
+const swap = async (data, x, y) => {
+    var temp = data[x];
+    data[x] = data[y];
+    data[y] = temp;
+    await sleep(2);
+}
+
+export const insertionSort = async (dataRef) => {
+    let data = dataRef.current;
+    let count = 0;
+    let sorted = 0;
+
+    for (let ii = 1; ii < data.length; ii++) {
+        if (data[sorted] > data[ii]) {
+            await swap(data, ii, sorted);
+            ii = ii -2;
+            sorted--;
+            count++;
+        } else {
+            sorted++;
+        }
+    }
+}
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- algorithm-visualizer/src/Pages/Sorting.js
+++ algorithm-visualizer/src/Pages/Sorting.js
@@ -1,21 +1,33 @@
 import './CSS/Sorting.css';
 import p5 from "p5";
 import React, { useEffect, useRef } from 'react';
+import Button from 'react-bootstrap/Button'
+import { insertionSort, sort } from "../Algorithms/SortingAlgorithms"
 
-const P5Sketch = () => {
+const getRandomData = (n) => {
+    if (n < 0) {return};
+    let randomData = [];
+    for (let ii = 0; ii < n; ii++) {
+        randomData[ii] = Math.floor(Math.random() * n) + 1;
+    }
+    return randomData;
+};
+
+const P5Sketch = ({dataRef}) => {
     const sketchRef = useRef();
 
     useEffect(() => {
+
         const sketch = (canvas) => {
             canvas.setup = () => {
-                canvas.createCanvas(1000, 1000);
+                canvas.createCanvas(1500, 800);
             };
 
             canvas.draw = () => {
                 canvas.background(100);
-                canvas.ellipse(canvas.width / 2, canvas.height / 2, 100, 100);
-                for (let ii = 0; ii < 10; ii++) {
-                    canvas.rect( ii * 100, canvas.height - 100, 55, 55 * -ii);
+                const data = dataRef.current;
+                for (let ii = 0; ii < data.length; ii++) {
+                    canvas.rect(ii*15, 800, 15, -data[ii]);
                 }
                 
             };
@@ -26,13 +38,20 @@ const P5Sketch = () => {
         return () => {
             p5_test.remove();
         };
-    }, []);
+    }, [dataRef]);
+    return <div ref={sketchRef}></div>;
 }
 
 export default function Sorting() {
+    const dataRef = useRef(getRandomData(100));
     return (
-        <div className="Sorting">
-            <P5Sketch/>
+        <div>
+            <div className="Sorting">
+                <P5Sketch dataRef={dataRef}/>
+            </div>
+            <div>
+                <Button onClick={() => insertionSort(dataRef)}>Sort</Button>
+            </div>
         </div>
     );
 }
