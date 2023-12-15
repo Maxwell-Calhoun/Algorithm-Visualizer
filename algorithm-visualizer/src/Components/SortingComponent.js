d796ccb6586a53905c9427cafe8ed3ae61f8b31c
Maxwell-Calhoun <mcalh16@gmail.com>
2023-12-15 01:03:27 -0700

    componentizing the P5 drawing

---
+++ algorithm-visualizer/src/Components/SortingComponent.js
@@ -0,0 +1,45 @@
+import p5 from "p5";
+import React, { useEffect, useRef } from 'react';
+
+let WIDTH;
+let HEIGHT;
+
+export const SortSketch = ({dataRef}) => {
+    const sketchRef = useRef();
+    useEffect(() => {
+        const sketch = (canvas) => {
+            canvas.setup = () => {
+                WIDTH = canvas.windowWidth;
+                HEIGHT = canvas.windowHeight - 100;
+                dataRef.current = getRandomData(WIDTH / 10);
+                canvas.createCanvas(WIDTH, HEIGHT);
+            };
+
+            canvas.draw = () => {
+                canvas.background(0);
+                const data = dataRef.current;
+                for (let ii = 0; ii < data.length; ii++) {
+                    canvas.rect(ii*10, HEIGHT, 10, -data[ii]);
+                }
+                
+            };
+        }
+        const p5_object = new p5(sketch, sketchRef.current);
+
+        return () => {
+            p5_object.remove();
+        };
+    }, [dataRef]);
+    return <div ref={sketchRef}></div>;
+}
+
+export default SortSketch;
+
+const getRandomData = (n) => {
+    if (n < 0) {return};
+    let randomData = [];
+    for (let ii = 0; ii < n; ii++) {
+        randomData[ii] = Math.floor(Math.random() * n) + 1;
+    }
+    return randomData;
+};
--- algorithm-visualizer/src/Pages/Sorting.js
+++ algorithm-visualizer/src/Pages/Sorting.js
@@ -1,55 +1,20 @@
 import './CSS/Sorting.css';
-import p5 from "p5";
-import React, { useEffect, useRef } from 'react';
-import Button from 'react-bootstrap/Button'
-import { insertionSort, sort } from "../Algorithms/SortingAlgorithms"
-
-const getRandomData = (n) => {
-    if (n < 0) {return};
-    let randomData = [];
-    for (let ii = 0; ii < n; ii++) {
-        randomData[ii] = Math.floor(Math.random() * n) + 1;
-    }
-    return randomData;
-};
-
-const P5Sketch = ({dataRef}) => {
-    const sketchRef = useRef();
-
-    useEffect(() => {
-
-        const sketch = (canvas) => {
-            canvas.setup = () => {
-                canvas.createCanvas(1500, 800);
-            };
-
-            canvas.draw = () => {
-                canvas.background(100);
-                const data = dataRef.current;
-                for (let ii = 0; ii < data.length; ii++) {
-                    canvas.rect(ii*15, 800, 15, -data[ii]);
-                }
-                
-            };
-        }
-
-        const p5_test = new p5(sketch, sketchRef.current);
-
-        return () => {
-            p5_test.remove();
-        };
-    }, [dataRef]);
-    return <div ref={sketchRef}></div>;
-}
+import React, { useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import { insertionSort } from "../Algorithms/SortingAlgorithms";
+import { SortSketch } from "../Components/SortingComponent";
 
 export default function Sorting() {
-    const dataRef = useRef(getRandomData(100));
+    const dataRef = useRef();
     return (
         <div>
             <div className="Sorting">
-                <P5Sketch dataRef={dataRef}/>
+                <SortSketch dataRef={dataRef}/>
             </div>
-            <div>
+            <div classname="selection">
+                <select>
+                    
+                </select>
                 <Button onClick={() => insertionSort(dataRef)}>Sort</Button>
             </div>
         </div>
