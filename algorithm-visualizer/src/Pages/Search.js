007dac4e68ab3cd8fa40fe8c2298646dc5e6de4e
Maxwell-Calhoun <mcalh16@gmail.com>
2023-12-16 16:13:03 -0700

    adding website information and putting search vis under construciton

--- algorithm-visualizer/src/Pages/Search.js
+++ algorithm-visualizer/src/Pages/Search.js
@@ -1,9 +1,21 @@
 import './CSS/Search.css';
+import { Footer } from '../Components/Footer';
+import wrench from '../img/wrench.png'
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+    const navigate = useNavigate();
     return (
         <div className="Search">
-
+            <img className='wrenchImg' src={wrench} alt="wrench"/>
+            <div className='Construction'>
+                <h1>Visualization Under Construction</h1>
+                <Button variant="secondary" size="lg" onClick={() => navigate("/")}>
+                Back Home
+                </Button>
+            </div>
+            <Footer/>
         </div>
     );
 }
