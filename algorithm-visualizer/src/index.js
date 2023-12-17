db99cfe1d88a06fdfc5d209e75a50fd3d4bf1fe5
Maxwell-Calhoun <mcalh16@gmail.com>
2023-12-17 00:33:19 -0700

    update to hash routing to accomdate better nav for deployment

--- algorithm-visualizer/src/index.js
+++ algorithm-visualizer/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 /*pages*/
@@ -13,14 +13,14 @@ import Error404 from './Pages/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/Algorithm-Visualizer'>
+  <Router>
     <Routes>
-      <Route path='/' element={<App/>}/>
+      <Route exact path='/' element={<App/>}/>
       <Route path='/Sorting' element={<Sorting/>}/>
       <Route path='/Search' element={<Search/>}/>
       <Route path='*' element={<Error404/>}/>
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
